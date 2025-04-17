85507b52564feb94cc8ce6626622a72f34a8c6d2
Dan Van Bueren <danvb10@gmail.com>
2025-04-17 13:14:09 +0200

    Scaffold

---
+++ gkorm/frontend/app/page.js
@@ -0,0 +1,12 @@
+import {Button} from "@mui/material";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+          <Button variant="contained">Hello world</Button>
+
+      </main>
+    </div>
+  );
+}
---
+++ gkorm/frontend/app/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+    typography: {
+        fontFamily: 'Roboto, sans-serif',
+    },
+});
+
+export default theme;
