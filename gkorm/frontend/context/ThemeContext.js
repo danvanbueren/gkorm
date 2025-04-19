5d7ee513d4a8cb7978e835c627e71a6da82a1e26
Daniel Van Bueren <danvb10@gmail.com>
2025-04-19 04:16:40 +0200

    Theme consolidation

--- gkorm/frontend/context/ThemeContext.js
+++ gkorm/frontend/context/ThemeContext.js
@@ -1,14 +1,14 @@
 'use client';
 
-import { createContext, useContext, useMemo, useState } from 'react';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import {createContext, useContext, useMemo, useState} from 'react';
+import {ThemeProvider, createTheme} from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
 const ThemeContext = createContext();
 
 export const useColorMode = () => useContext(ThemeContext);
 
-export default function ThemeContextProvider({ children }) {
+export default function ThemeContextProvider({children}) {
     const [mode, setMode] = useState('dark');
 
     const colorMode = useMemo(() => ({
@@ -17,22 +17,27 @@ export default function ThemeContextProvider({ children }) {
         },
     }), []);
 
-    const theme = useMemo(() =>
-        createTheme({
-            palette: {
-                mode,
+    const theme = useMemo(() => createTheme({
+        palette: {
+            mode, custom: {
+                borderColor: mode === 'dark' ? '#111' : '#333',
+                headingBackground: mode === 'dark' ? '#424242' : '#bdbdbd',
+                signatureBackground: mode === 'dark' ? '#2c2c2c' : '#dfdfdf',
+                salmonBackground: mode === 'dark' ? '#ca958a' : '#d8998c',
+                defaultTextColor: mode === 'dark' ? '#fff' : '#000',
+                infoBackground: mode === 'dark' ? '#252525' : '#dfdfdf',
+                clickableBackground: mode === 'dark' ? '#c9d8ff' : '#c9d8ff',
+                clickableBackgroundActive: mode === 'dark' ? '#789fff' : '#789fff',
             },
-            typography: {
-                fontFamily: 'Roboto, sans-serif',
-            },
-        }), [mode]);
+        }, typography: {
+            fontFamily: 'Roboto, sans-serif',
+        },
+    }), [mode]);
 
-    return (
-        <ThemeContext.Provider value={colorMode}>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                {children}
-            </ThemeProvider>
-        </ThemeContext.Provider>
-    );
+    return (<ThemeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            {children}
+        </ThemeProvider>
+    </ThemeContext.Provider>);
 }
