2bc0ecc0328a16f509354cd20c687f9cc85ae75c
Daniel Van Bueren <danvb10@gmail.com>
2025-04-20 02:35:16 +0200

    Pages support

--- gkorm/frontend/next.config.mjs
+++ gkorm/frontend/next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    // optionally set a basePath and assetPrefix if deploying to a subpath
+    basePath: '/<repo-name>',
+    assetPrefix: '/<repo-name>/',
+};
+
+module.exports = nextConfig
 
 export default nextConfig;
