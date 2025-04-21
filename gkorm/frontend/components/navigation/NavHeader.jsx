5e3368f124feeb12919e428f55298b36524b1383
Daniel Van Bueren <danvb10@gmail.com>
2025-04-21 15:35:08 +0200

    Routing fix & aesthetics

--- gkorm/frontend/components/navigation/NavHeader.jsx
+++ gkorm/frontend/components/navigation/NavHeader.jsx
@@ -2,12 +2,12 @@
 
 import React from 'react';
 import {AppBar, Toolbar, Typography, Box, Button, IconButton, Avatar, Container, MenuItem, Menu} from '@mui/material';
-import { Brightness4, Brightness7 } from '@mui/icons-material';
+import {Brightness4, Brightness7} from '@mui/icons-material';
 import {useTheme} from "@mui/material";
 import {useColorMode} from "@/context/ThemeContext";
 import nextConfig from "@/next.config.mjs";
 
-export default function NavHeader({ user = { name: 'Undefined Session' } }) {
+export default function NavHeader({user = {name: 'Undefined Session'}}) {
 
     const theme = useTheme();
     const colorMode = useColorMode();
@@ -22,113 +22,114 @@ export default function NavHeader({ user = { name: 'Undefined Session' } }) {
     };
 
     return (
-        <AppBar
-            position="sticky"
-            color="primary"
-            elevation={4}
-            sx={{
-                justifyContent: 'space-between',
-                borderRadius: '1rem',
-                padding: '0.2rem',
-                marginY: '1rem',
-
-            }}
-        >
-            <Container
-                maxWidth="xl"
+        <Box sx={{padding: '1rem'}}>
+            <AppBar
+                position="sticky"
+                color="primary"
+                elevation={4}
+                sx={{
+                    justifyContent: 'space-between',
+                    borderRadius: '1rem',
+                    padding: '0.2rem',
+                }}
             >
-                <Toolbar
-                    disableGutters
-                    sx={{
-                        justifyContent: 'space-between',
-                    }}
+                <Container
+                    maxWidth="xl"
                 >
-                    {/* Logo or Title */}
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        component="a"
-                        href={nextConfig.basePath + "/"}
+                    <Toolbar
+                        disableGutters
                         sx={{
-                            mr: 2,
-                            display: 'flex',
-                            fontWeight: 700,
-                            letterSpacing: '.1rem',
-                            color: 'inherit',
-                            textDecoration: 'none',
-                            userSelect: 'none',
+                            justifyContent: 'space-between',
                         }}
                     >
-                        gkorm
-                    </Typography>
-
-                    <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                    {/* Links */}
-                    <Box sx={{ display: 'flex', gap: 2 }}>
-                        <Button color="inherit" href={nextConfig.basePath + "/mission/example"}>Example Mission</Button>
-                        <Button color="inherit" href="/" disabled>Missions</Button>
-                    </Box>
-
-                    {/* Right: Theme toggle + user info */}
-
-                        <IconButton color="inherit" onClick={colorMode.toggleColorMode}>
-                            {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
-                        </IconButton>
-
-                        <IconButton
-                            id="basic-button"
-                            aria-controls={open ? 'basic-menu' : undefined}
-                            aria-haspopup="true"
-                            aria-expanded={open ? 'true' : undefined}
-                            onClick={handleClick}
+                        {/* Logo or Title */}
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="a"
+                            href={nextConfig.basePath + "/"}
                             sx={{
-                                borderRadius: 4,
-                                backgroundColor: open ? 'action.selected' : 'transparent',
-                                '&:hover': {
-                                    backgroundColor: open ? 'action.selected' : 'action.hover',
-                                },
+                                mr: 2,
+                                display: 'flex',
+                                fontWeight: 700,
+                                letterSpacing: '.1rem',
                                 color: 'inherit',
+                                textDecoration: 'none',
+                                userSelect: 'none',
                             }}
                         >
+                            gkorm
+                        </Typography>
 
-                            <Avatar
-                                sx={{
-                                    width: 32, height: 32
-                                }}
-                            >
-                                {user.name[0]}
-                            </Avatar>
+                        <Box sx={{display: 'flex', alignItems: 'center', gap: 1}}>
+                            {/* Links */}
+                            <Box sx={{display: 'flex', gap: 2}}>
+                                <Button color="inherit" href={nextConfig.basePath + "/mission/example"}>Example
+                                    Mission</Button>
+                                <Button color="inherit" href="/" disabled>Missions</Button>
+                            </Box>
 
-                            <Typography
-                                variant="button"
+                            {/* Right: Theme toggle + user info */}
+
+                            <IconButton color="inherit" onClick={colorMode.toggleColorMode}>
+                                {theme.palette.mode === 'dark' ? <Brightness7/> : <Brightness4/>}
+                            </IconButton>
+
+                            <IconButton
+                                id="basic-button"
+                                aria-controls={open ? 'basic-menu' : undefined}
+                                aria-haspopup="true"
+                                aria-expanded={open ? 'true' : undefined}
+                                onClick={handleClick}
                                 sx={{
-                                    ml: 1,
+                                    borderRadius: 4,
+                                    backgroundColor: open ? 'action.selected' : 'transparent',
+                                    '&:hover': {
+                                        backgroundColor: open ? 'action.selected' : 'action.hover',
+                                    },
                                     color: 'inherit',
                                 }}
                             >
-                                {user.name}
-                            </Typography>
 
-                        </IconButton>
+                                <Avatar
+                                    sx={{
+                                        width: 32, height: 32
+                                    }}
+                                >
+                                    {user.name[0]}
+                                </Avatar>
 
-                        <Menu
-                            id="basic-menu"
-                            anchorEl={anchorEl}
-                            open={open}
-                            onClose={handleClose}
-                            PaperProps={{
-                                sx: {
-                                    minWidth: anchorEl ? anchorEl.clientWidth : undefined, // match button width
-                                },
-                            }}
-                        >
-                            <MenuItem onClick={handleClose}>Options1</MenuItem>
-                            <MenuItem onClick={handleClose}>Options2</MenuItem>
-                            <MenuItem onClick={handleClose}>Options3</MenuItem>
-                        </Menu>
-                    </Box>
-                </Toolbar>
-            </Container>
-        </AppBar>
+                                <Typography
+                                    variant="button"
+                                    sx={{
+                                        ml: 1,
+                                        color: 'inherit',
+                                    }}
+                                >
+                                    {user.name}
+                                </Typography>
+
+                            </IconButton>
+
+                            <Menu
+                                id="basic-menu"
+                                anchorEl={anchorEl}
+                                open={open}
+                                onClose={handleClose}
+                                PaperProps={{
+                                    sx: {
+                                        minWidth: anchorEl ? anchorEl.clientWidth : undefined, // match button width
+                                    },
+                                }}
+                            >
+                                <MenuItem onClick={handleClose}>Options1</MenuItem>
+                                <MenuItem onClick={handleClose}>Options2</MenuItem>
+                                <MenuItem onClick={handleClose}>Options3</MenuItem>
+                            </Menu>
+                        </Box>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </Box>
     );
 }
