cb40ef435e7ecb26acbabb47876988a508a62fe5
Daniel Van Bueren <danvb10@gmail.com>
2025-04-21 15:34:22 +0200

    Disable SSG

--- gkorm/frontend/next.config.mjs
+++ gkorm/frontend/next.config.mjs
@@ -1,20 +1,22 @@
 /** @type {import('next').NextConfig} */
 
-/*
+
 
 // Dev
 const nextConfig = {
     basePath: '',
 };
 
-*/
+/*
 
-// Static site generation (GitHub Pages)
+// Only for static site generation (GitHub Pages)
+
 const nextConfig = {
     output: 'export',
     basePath: '/gkorm',
     assetPrefix: '/gkorm/',
 };
 
+ */
 
 export default nextConfig;
