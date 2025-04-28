6614b7c8a0f3ac7e9fdcb90412000eaad99d8ac4
Daniel Van Bueren <danvb10@gmail.com>
2025-04-28 15:17:36 +0200

    Remove SSG - not relevant for this project

--- gkorm/frontend/next.config.mjs
+++ gkorm/frontend/next.config.mjs
@@ -1,22 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-
-
 // Dev
 const nextConfig = {
     basePath: '',
 };
-
-/*
-
-// Only for static site generation (GitHub Pages)
-
-const nextConfig = {
-    output: 'export',
-    basePath: '/gkorm',
-    assetPrefix: '/gkorm/',
-};
-
- */
 
 export default nextConfig;
