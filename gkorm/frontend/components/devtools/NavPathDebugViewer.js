5a9d1c56c2e61850727c53b3d08177bfae4f4442
Daniel Van Bueren <daniel.vanbueren@proton.me>
2025-09-03 16:12:58 +0200

    Fragment nav path display into new component

---
+++ gkorm/frontend/components/devtools/NavPathDebugViewer.js
@@ -0,0 +1,29 @@
+import {Box} from "@mui/material";
+import {useSpaRouter} from "@/context/SpaRouter";
+
+export default function NavPathDebugViewer() {
+
+    const {currentPath} = useSpaRouter();
+    const pathAsArray = currentPath.split("/");
+
+    return (
+        <Box position='relative'>
+            <Box position='absolute' right='0' top='10rem'>
+                <table style={{border: '1px solid yellow', borderCollapse: 'collapse', background: 'black', opacity: 0.5, color: 'yellow'}}>
+                    <tbody>
+                    <tr style={{border: '1px solid yellow', borderCollapse: 'collapse'}}>
+                        {pathAsArray.map((item, i) => {
+                            return (<td key={i+'1'} style={{width: '5rem', border: '1px solid yellow', borderCollapse: 'collapse', padding: '1rem'}}>{i}</td>)
+                        })}
+                    </tr>
+                    <tr>
+                        {pathAsArray.map((item, i) => {
+                            return (<td key={i+'2'} style={{border: '1px solid yellow', borderCollapse: 'collapse', padding: '1rem'}}>'{item}'</td>)
+                        })}
+                    </tr>
+                    </tbody>
+                </table>
+            </Box>
+        </Box>
+    )
+}
