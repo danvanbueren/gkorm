f03b70e690388e5ded3289287c5bbff9d0d1ef87
Daniel Van Bueren <daniel.vanbueren@proton.me>
2025-09-09 13:59:11 +0200

    Split enums to separate file, add relationships

--- gkorm/api/app/database_models.py
+++ gkorm/api/app/database_models.py
@@ -1,57 +1,12 @@
 """Base definitions for database tables."""
-import enum
 
 from sqlalchemy import Column, Integer, String, DateTime, Enum, ForeignKey
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy.sql import func
 
+from app.database_enums import *
+
 Base = declarative_base()
-
-class CrewPositions(enum.Enum):
-    PILOT = "pilot"
-    FLIGHT_ENGINEER = "flight_engineer"
-    TACTICAL_DIRECTOR = "tactical_director"
-    FIGHTER_ALLOCATOR = "fighter_allocator"
-    WEAPONS_CONTROLLER = "weapons_controller"
-    FIGHTER_ALLOCATOR_WEAPONS_CONTROLLER = "fighter_allocator_weapons_controller"
-    SURVEILLANCE_CONTROLLER = "surveillance_controller"
-    PASSIVE_CONTROLLER = "passive_controller"
-    SURVEILLANCE_OPERATOR = "surveillance_operator"
-    SYSTEM_TECHNICIAN = "system_technician"
-    COMMUNICATIONS_TECHNICIAN = "communications_technician"
-    RADAR_TECHNICIAN = "radar_technician"
-    UNQUALIFIED = "unqualified"
-    PASSENGER = "passenger"
-
-class CrewPositionModifiers(enum.Enum):
-    EVALUATOR = "evaluator"
-    INSTRUCTOR = "instructor"
-    LINK = "link"
-    BASIC = "basic"
-
-class Units(enum.Enum):
-    FLYING_SQUADRON_1 = "flying_squadron_1"
-    FLYING_SQUADRON_2 = "flying_squadron_2"
-    AIRCREW_TRAINING_SQUADRON = "aircrew_training_squadron"
-    UNASSIGNED = "unassigned"
-
-class GlobalPermissions(enum.Enum):
-    BASIC = "basic"
-    SQUADRON_DIRECTOR_OF_OPERATIONS = "squadron_director_of_operations"
-    SQUADRON_COMMANDER = "squadron_commander"
-    OPERATIONS_WING_COMMANDER = "operations_wing_commander"
-    SYSTEM_ADMINISTRATOR = "system_administrator"
-
-class RiskLevels(enum.Enum):
-    LOW = "low"
-    MEDIUM = "medium"
-    HIGH = "high"
-
-class WorksheetTypes(enum.Enum):
-    MISSION_PLANNING_RISK_ASSESSMENT = "mission_planning_risk_assessment"
-    DAY_OF_MISSION_RISK_ASSESSMENT = "day_of_mission_risk_assessment"
-    PERSONAL_RISK_ASSESSMENT = "personal_risk_assessment"
-    PILOT_PROFICIENCY_RISK_ASSESSMENT = "pilot_proficiency_risk_assessment"
 
 class BaseModel(Base):
     __abstract__ = True
@@ -62,11 +17,13 @@ class BaseModel(Base):
 class UsersTable(BaseModel):
     __tablename__ = "users_table"
     amis_id = Column(Integer, unique=True, index=True, nullable=False)
+    rank = Column(Enum(Ranks), nullable=True)
     given_name = Column(String(255), nullable=True)
     family_name = Column(String(255), nullable=True)
     crew_position = Column(Enum(CrewPositions), nullable=True)
     crew_position_modifier = Column(Enum(CrewPositionModifiers), nullable=True)
     assigned_unit = Column(Enum(Units), nullable=True)
+    missions = relationship("MissionsTable", back_populates="owner")
 
 class UserPermissionsTable(BaseModel):
     __tablename__ = "user_permissions_table"
@@ -77,12 +34,14 @@ class MissionsTable(BaseModel):
     __tablename__ = "missions_table"
     mission_number = Column(String(255), index=True, unique=True, nullable=False)
     FKEY_users_TABLE_owner_id = Column(Integer, ForeignKey("users_table.PKEY_id"), nullable=False)
+    owner = relationship("UsersTable", back_populates="missions")
 
 class MemberMissionAssignmentsTable(BaseModel):
     __tablename__ = "member_mission_assignments_table"
     FKEY_missions_TABLE_parent_id = Column(Integer, ForeignKey("missions_table.PKEY_id"), nullable=False)
     FKEY_users_TABLE_member_id = Column(Integer, ForeignKey("users_table.PKEY_id"), nullable=False)
     crew_position_override = Column(Enum(CrewPositions))
+    crew_position_modifier_override = Column(Enum(CrewPositionModifiers))
 
 class WorksheetsTable(BaseModel):
     __tablename__ = "worksheets_table"
