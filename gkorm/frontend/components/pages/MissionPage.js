2f4603b67771f7701ca77dc257ea81713500136d
Daniel Van Bueren <daniel.vanbueren@proton.me>
2025-09-10 15:13:18 +0200

    Integrate API & error handling

--- gkorm/frontend/components/pages/MissionPage.js
+++ gkorm/frontend/components/pages/MissionPage.js
@@ -1,6 +1,6 @@
 'use client';
 
-import {Box, Grid} from "@mui/material";
+import {Box, Button, Grid, Typography} from "@mui/material";
 import NavHeader from "@/components/navigation/NavHeader";
 import MissionPlanningRiskAssessment from "@/components/constructedWorksheets/MissionPlanningRiskAssessment";
 import MissionFlowNav from "@/components/navigation/MissionFlowNav";
@@ -10,14 +10,45 @@ import PilotProficiencyRiskAssessment from "@/components/constructedWorksheets/P
 import ProcessFlow from "@/components/constructedWorksheets/ProcessFlow";
 import CrewList from "@/components/constructedWorksheets/CrewList";
 import {RequireAuth} from "@/components/utility/RequireAuth";
+import {useEffect, useState} from "react";
+import {useSpaRouter} from "@/context/SpaRouter";
 
 export default function MissionPage({requestedView}) {
 
-    const missionData = {
-        number: 'AJ1234M',
-        date: '12-Nov-25',
-        acNameIdNumber: 'OF-3 John Doe, 012345'
+    const {navigate, currentPath} = useSpaRouter();
+    const pathAsArray = currentPath.split("/");
+    const missionNumberFromUrl = pathAsArray[2]
+
+    const [missionData, setMissionData] = useState()
+    const [dataLoadError, setDataLoadError] = useState()
+
+    // API
+    const missionDataFromApi = async () => {
+        const controller = new AbortController()
+        const timeoutId = setTimeout(() => controller.abort(), 10000)
+
+        try {
+            setDataLoadError('')
+            const response = await fetch(`http://localhost:8000/missions/get/${missionNumberFromUrl}`, {
+                signal: controller.signal,
+            })
+
+            if (!response.ok)
+                throw new Error('Failed to fetch data - ' + response.statusText)
+
+            const data = await response.json()
+            setMissionData(data.content)
+        } catch (error) {
+            setDataLoadError(error.message)
+        } finally {
+            clearTimeout(timeoutId)
+        }
     }
+
+    // TABLE - Initial API fetch
+    useEffect(() => {
+        missionDataFromApi()
+    }, [])
 
     const currentView = () => {
         switch (requestedView) {
@@ -42,30 +73,58 @@ export default function MissionPage({requestedView}) {
                 <NavHeader/>
             </Box>
             <Grid container spacing={2} width={'100%'} height={'85dvh'}>
-                <Grid size={{xs: 12, lg: 3}}>
-                    <Box
-                        sx={{
-                            maxHeight: '80dvh',
-                            overflow: 'auto',
-                            padding: '1rem',
-                        }}
-                    >
-                        <MissionFlowNav
-                            missionNumber={missionData.number}
-                            acNameIdNumber={missionData.acNameIdNumber}
-                            currentView={requestedView}
-                        />
-                    </Box>
-                </Grid>
-                <Grid size={{xs: 12, lg: 9}}>
-                    <Box
-                        sx={{
-                            maxHeight: '80dvh', overflow: 'auto', padding: '1rem',
-                        }}
-                    >
-                        {currentView()}
-                    </Box>
-                </Grid>
+                {
+                    (!missionData && dataLoadError) ?
+                        <Grid size={{xs: 12}} sx={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100%'}}>
+                            <Typography
+                                variant='h3'
+                                sx={{ mb: 2 }}
+                            >
+                                Failed to load mission!
+                            </Typography>
+
+                            <Typography
+                                variant='body2'
+                                sx={{ mb: 3 }}
+                            >
+                                {dataLoadError}
+                            </Typography>
+
+                            <Button
+                                variant="contained"
+                                size='large'
+                                sx={{ px: 3, py: 2, mb: '20dvh' }}
+                                onClick={() => navigate('/')}
+                            >
+                                Return to homepage
+                            </Button>
+                        </Grid> :
+                        <>
+                            <Grid size={{xs: 12, lg: 3}}>
+                                <Box
+                                    sx={{
+                                        maxHeight: '80dvh',
+                                        overflow: 'auto',
+                                        padding: '1rem',
+                                    }}
+                                >
+                                    <MissionFlowNav
+                                        missionData={missionData}
+                                        currentView={requestedView}
+                                    />
+                                </Box>
+                            </Grid>
+                            <Grid size={{xs: 12, lg: 9}}>
+                                <Box
+                                    sx={{
+                                        maxHeight: '80dvh', overflow: 'auto', padding: '1rem',
+                                    }}
+                                >
+                                    {currentView()}
+                                </Box>
+                            </Grid>
+                        </>
+                }
             </Grid>
         </RequireAuth>
     );
