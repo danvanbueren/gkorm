962ec39a3958ca0fc94ad92cb51195714cb1cf37
Daniel Van Bueren <daniel.vanbueren@proton.me>
2025-09-13 14:13:35 +0200

    Rename for clarity

--- gkorm/api/app/response_schemas.py
+++ gkorm/api/app/response_schemas.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from app.database_enums import Ranks
 
 
-class UserOut(BaseModel):
+class UserSchema(BaseModel):
     PKEY_id: int
     rank: Optional[Ranks]
     given_name: Optional[str]
@@ -13,16 +13,16 @@ class UserOut(BaseModel):
     amis_id: int
     model_config = ConfigDict(from_attributes=True)
 
-class MissionOut(BaseModel):
+class MissionSchema(BaseModel):
     PKEY_id: int
     mission_number: str
     status: str
     execution_date: Optional[datetime]
     FKEY_users_TABLE_owner_id: int
-    owner: Optional[UserOut] = None
+    owner: Optional[UserSchema] = None
     model_config = ConfigDict(from_attributes=True, use_enum_values=True)
 
-class ListResponse(BaseModel):
+class MissionListResponseSchema(BaseModel):
     status: int
     message: str
-    content: list[MissionOut]
+    content: list[MissionSchema]
